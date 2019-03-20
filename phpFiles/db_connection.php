d64d978c2d30c954394822b1e6c8fd365d36f7af
DikshaC <dchhabra1995@gmail.com>
2019-03-20 13:55:51 -0500

    db_connection added

---
+++ phpFiles/db_connection.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dchha
+ * Date: 3/20/2019
+ * Time: 12:30 PM
+ */
+
+//require('db_connection.php')
+//use global $connection; in every file
+
+$servername = "localhost";
+$username = "diksha";
+$password = "diksha";
+$dbname = "stock_data";
+
+// Create connection
+global $connection;
+
+$connection= new mysqli($servername, $username, $password, $dbname);
+// Check connection
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+?>
